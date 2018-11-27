5b5a622c9abd33c758753bf4958f2da1a8394f8c
levinyi <dushiyi319@163.com>
2018-11-27 16:04:17 +0800

    ok

---
+++ blast.py
@@ -0,0 +1,41 @@
+
+
+def score(a, b):
+    score = 0
+    lst = ['AC', 'GT', 'CA', 'TG']
+    if a == b:
+        score += 2
+    elif a + b in lst:
+        score += -5
+    else:
+        score += -7
+    return score
+
+
+def BLAST(seq1, seq2):
+    l1 = len(seq1)
+    l2 = len(seq2)
+    GAP = -5
+    scores = []
+    point = []
+    for j in range(l2 + 1):
+        print j
+        if j == 0 :# first position
+            line1 = [0]
+            line2 = [0]
+            for i in range(1,l1+1):
+                line1.append(GAP*i)
+                line2.append(2)
+        else:
+            line1 = []
+            line2 = []
+            line1.append(GAP*j)
+            line2.append(3)
+        scores.append(line1)
+        point.append(line2)
+    print scores
+    print point
+
+a = 'ATAAATGTATTTATGATTGATGTATTTTTTTACATGGATTTTTTCGTCCCAAAAAGGG'
+b = 'ATAAATGTATTTATGATTGTTAACTTTTTTTAGGGGGATTTTTTCGTCCCAAAAAGGG'
+BLAST(a,b)
