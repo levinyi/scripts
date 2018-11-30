4f0e3a99b255bff06f86d182c6d64aece90aa9f6
dushiyi <dushiyi319@163.com>
2018-11-30 18:21:50 +0800

    some changes

--- blast.py
+++ blast.py
@@ -1,4 +1,38 @@
+from Bio.SubsMat import MatrixInfo
+def score_match(pair, matrix):
+    if pair not in matrix:
+        return matrix[(tuple(reversed(pair)))]
+    else:
+        return matrix[pair]
 
+def score_pairwise(seq1, seq2, matrix, gap_s, gap_e):
+    score = 0
+    gap = False
+    for i in range(len(seq1)):
+        pair = (seq1[i], seq2[i])
+        print pair
+        if not gap:
+            if '-' in pair:
+                gap = True
+                score += gap_s
+            else:
+                score += score_match(pair, matrix)
+        else:
+            if '-' not in pair:
+                gap = False
+                score += score_match(pair, matrix)
+            else:
+                score += gap_e
+    return score
+# seq1 = 'PAVKDLGAEG-ASDKGT--SHVVY----------TI-QLASTFE'
+# seq2 = 'PAVEDLGATG-ANDKGT--LYNIYARNTEGHPRSTV-QLGSTFE'
+seq1 = 'ATAAATGTATTTATGATTGATGTATTTTTTTACATGGATTTTTTCGTCCCAAAAAGGG'
+seq2 = 'ATAAATGTATTTATGATTGTTAACTTTTTTTAGGGGGATTTTTTCGTCCCAAAAAGGG'
+print seq1
+print seq2
+blosum = MatrixInfo.blosum62
+
+print score_pairwise(seq1, seq2, blosum, -5, -1)
 
 def score(a, b):
     score = 0
@@ -11,36 +45,19 @@ def score(a, b):
         score += -7
     return score
 
-def PAM_matrix_score(a,b):
-    score = 0
-
-    return score
-
-def Blosum_matrix_score(a,b):
-
-
-    score = 0
-    lst = []
-    
-    if sss:
-        lanbuda = 4
-    else:
-        lanbuda = 20
-    S = (1/lanbuda)log(pij/qi*qj)
-    return score
-
-def BLAST(seq1, seq2):
+def BLAST(seq1, seq2,matrix):
     l1 = len(seq1)
     l2 = len(seq2)
     GAP = -5
+    gap_e = -1
     scores = []
     point = []
-    for j in range(l2 + 1):
+    for j in range(len(seq2) + 1):
         # print j
         if j == 0:  # first position
             line1 = [0]
             line2 = [0]
-            for i in range(1, l1 + 1):
+            for i in range(1, len(seq1) + 1):
                 line1.append(GAP * i)
                 line2.append(2)
         else:
@@ -52,11 +69,13 @@ def BLAST(seq1, seq2):
         point.append(line2)
 
     for j in range(1, l2 + 1):
+        pair = (seq1[i], seq2[i])
         letter2 = seq2[j - 1]
         # print letter2
         for i in range(1, l1 + 1):
             letter1 = seq1[i - 1]
-            diagonal_score = score(letter1, letter2) + scores[j - 1][i - 1]
+            # diagonal_score = score(letter1, letter2) + scores[j - 1][i - 1]
+            diagonal_score = score_match(pair,matrix) + scores[j - 1][i - 1]
             # print diagonal_score
             left_score = GAP + scores[j][i - 1]
             up_score = GAP + scores[j - 1][i]
@@ -103,3 +122,4 @@ b = 'ATAAATGTATTTATGATTGTTAACTTTTTTTAGGGGGATTTTTTCGTCCCAAAAAGGG'
 # b = 'ATAATTCTAT'
 
 BLAST(a, b)
+'''
