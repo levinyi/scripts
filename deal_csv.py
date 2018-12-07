2f7724678191a8ff9f4a333bb6198e790e9ffeb2
dushiyi <dushiyi319@163.com>
2018-12-07 16:44:05 +0800

    del

--- deal_csv.py
+++
@@ -1,9 +0,0 @@
-import sys
-import csv
-with open(sys.argv[1],"rb") as csvfile:
-	# spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-	spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-	print spamreader
-	for row in spamreader:
-		print row
-		# print ','.join(row)
