7b1ab4ab92e15c711456807471fbe8d6b3521735
dushiyi <dushiyi319@163.com>
2018-12-11 16:20:13 +0800

    update

--- compare.py
+++ compare.py
@@ -9,10 +9,16 @@ def _argparse():
     parser.add_argument('-al', action='store', default=1, dest='file1_colum', type=int, help='Set a colum to use, default first line.')
     parser.add_argument('-b', '--bfile', action='store', dest='file2', required=True, help='second file')
     parser.add_argument('-bl', action='store', default=1, dest='file2_colum', type=int, help='Set a colum to use, default first line.')
+
     parser.add_argument('-c', '--common', action='store_true', dest='common', default=False, help='show common or not. Default [ False ]. show this common result.')
     parser.add_argument('-u', '--union',  action='store_true', dest='union', default=False, help='show union or not. Default [ False ]. will not print this union result.')
     parser.add_argument('-l', '--leftside', action='store_true', dest='leftside', default=False, help='show rest of A file. Default: False. will not print this leftside result.')
     parser.add_argument('-r', '--rightside', action='store_true', dest='rightside', default=False, help='Show rest of B file. Default [ False ]. Will not print this rightside result.')
+    parser.add_argument('-cname', '--cfilename', action='store', dest='common_op_file', help='write common result to output file. Default: print this common result to screen.')
+    parser.add_argument('-uname', '--ufilename', action='store', dest='union_op_file',  help='write  union result to output file. Default: print this union result to screen.')
+    parser.add_argument('-lname', '--lfilename', action='store', dest='leftside_op_file', help='write leftside result to output file. Default: print this leftside result to screen.')
+    parser.add_argument('-rname', '--rfilename', action='store', dest='rightside_op_file', help='write rightside result to ouput file. Default: print this rightside result to screen.')
+    
     parser.add_argument('-g', '--graph', action='store_true', dest='graph', default=False, help='Draw a venn graph. Default [ False ]. will not print venn graph.')
 
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
@@ -41,17 +47,34 @@ def main():
     right_diff = list(set(file_list_b).difference(set(file_list_a)))
 
     # print basic information.
-    print("common:%s" % len(intersection))
 
+    print("\t Number of elements \t number of unique elements")
+    print("File A:\t %s\t%s" % (number_of_elements_a, number_of_uniq_element_a))
+    print("File B:\t %s\t%s" % (number_of_elements_b, number_of_uniq_element_b))
+    print("Overall number of unique elements: %s" % (len(union)))
+    print("\n-----------------------------------------------------\n")
+    print("left\tmiddle\tright")
+    print("%s\t%s\t%s\n" % (len(left_diff), len(intersection), len(right_diff)))
     # print additional info.
     if parser.common:
-        print len(intersection)
+        print("intersection list :")
+        for line in intersection:
+            print line
     if parser.leftside:
-        print len(left_diff)
+        print("File A list:")
+        for line in left_diff:
+            print line
+        # print len(left_diff)
     if parser.rightside:
-        print len(right_diff)
+        print("File B list:")
+        for line in right_diff:
+            print line
+        # print len(right_diff)
     if parser.union:
-        print len(union)
+        print("File A+B union list:")
+        for line in union:
+            print line
+        # print len(union)
     if parser.graph:
         with open("file_a", "w") as f1, open("file_b", "w") as f2:
             for each in file_list_a:
