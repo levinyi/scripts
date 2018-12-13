910eb825505462f4d02625e95a4ce674d9d92d7f
dushiyi <dushiyi319@163.com>
2018-12-13 11:36:19 +0800

    finished

--- download_pdf.py
+++ download_pdf.py
@@ -1,44 +1,28 @@
+import os
+import sys
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://openstax.org/details/books/biology"
-'''
-headers = {
-        'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Connection' : 'Keep-Alive',
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-}
-html = requests.get(url,headers = headers)
-print(html.text)
-soup = BeautifulSoup(html.text,"lxml")
-print(soup.select('a'))
-'''
 
-from selenium import webdriver  
-# driver = webdriver.Chrome('/cygene/script/test/chromedriver')
-driver = webdriver.PhantomJS('/cygene/software/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
-driver.get(url)
-print(driver.current_url)
-page = requests.get(driver.current_url,headers={'User-Agent':'Mozilla/5.0'})
-print(page.text)
+# url = "https://www.physicsandmathstutor.com/chemistry-revision/a-level-edexcel-ial/unit-5/"
+url = sys.argv[1]
+html = requests.get(url)
 
-
-'''
 if html.status_code == 200:
-    # print "yes"
     soup = BeautifulSoup(html.text, "html.parser")
-    # print soup.p
-    # print soup.a
-    # print soup.body
-    print soup.select("div class")
+    for link in soup.find_all('a'):
+        new_link = str(link.get('href'))
+        if new_link.endswith('.pdf'):
+            pdf_name = new_link.split("/")[-1].replace(" ", "_").replace(",","").replace("&","and")
+            if os.path.exists(pdf_name):
+                pdf_name = pdf_name.replace(".pdf","_1.pdf")
+            
+            # print pdf_name
+            
+            pdf_r = requests.get(new_link,stream=True,verify=False)
 
-    print soup.find_all("span")
-    print soup.find_all("scripts")
-
-
-import urllib2
-
-response = urllib2.urlopen(url)
-print response.read()
-'''
+            with open(pdf_name, "wb") as pdf_file:
+                for chunk in pdf_r.iter_content(chunk_size=1024):
+                    if chunk: # filter out keep-alive new chunks
+                        pdf_file.write(chunk)
+                        pdf_file.flush()
