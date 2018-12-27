18c19cd5e39fe497d30c245e9b17b0b9e433d3d8
dushiyi <dushiyi319@163.com>
2018-12-27 14:15:40 +0800

    some changes

--- extract.FastX.py
+++ extract.FastX.py
@@ -5,20 +5,21 @@ import argparse
 import gzip
 
 
-def _argparse():
-    pass
-
-
-def deal_fastq(fastq, id_list):
+def deal_fastq(fastq, id_list, file_true):
     for record in SeqIO.parse(fastq, "fastq"):
         if record.id in id_list:
-            SeqIO.write(record, "extract.id.fastq", "fastq")
+            if file_true == True:
+                SeqIO.write(record, "extract.id.fastq", "fastq")
+            elif file_true == False:
+                SeqIO.write(record, sys.stdout, "fastq")
 
 
-def deal_fasta(fasta, chrome, start=0, end=0):
+def deal_fasta(fasta, chrome, flag, start=0, end=0):
     for record in SeqIO.parse(fasta, "fasta"):
-        if record.id == chrome:
+        if record.id == chrome and flag == 'Ture':
             print record.seq[start - 1: end]  # start include; ends exclude
+        elif record.id == chrome and flag == 'False':
+            print record.seq
 
 # check file header.
 if sys.argv[1].endswith(".gz"):
@@ -30,14 +31,16 @@ else:
 if sys.argv[1].replace(".gz", "").endswith("fa") or sys.argv[1].replace(".gz", "").endswith("fasta"):
     # extract specific region sequence.
     if len(sys.argv) == 5:
+        flag = 'Ture'
         chrome = sys.argv[2]
         starts = int(sys.argv[3])
         ends = int(sys.argv[4])
-        deal_fasta(header, chrome, starts, ends)
+        deal_fasta(header, chrome, flag, starts, ends)
     # extract whole chrome sequence.
     elif len(sys.argv) == 3:
+        flag = 'False'
         chrome = sys.argv[2]
-        deal_fasta(header, chrome)
+        deal_fasta(header, chrome,flag)
     # argument error
     else:
         sys.exit("error with input format. see example.")
@@ -47,11 +50,13 @@ elif sys.argv[1].replace(".gz", "").endswith("fq") or sys.argv[1].replace(".gz",
     # read id is a file
     if os.path.isfile(sys.argv[2]) :
         id_list = [line.rstrip("\n") for line in open(sys.argv[2], "r")]
+        file_true = True
     # specific read id
     else:
         id_list = []
         id_list.append(sys.argv[2])
-    deal_fastq(header, id_list)
+        file_true = False
+    deal_fastq(header, id_list, file_true)
 
 # input format error. do not support other format file.
 else:
