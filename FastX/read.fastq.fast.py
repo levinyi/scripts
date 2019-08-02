5fd3844db0d585d934fb9b4feb7e0a3fab1a0098
dushiyi <dushiyi319@163.com>
2019-08-02 13:38:05 +0800

    some changes

---
+++ FastX/read.fastq.fast.py
@@ -0,0 +1,35 @@
+""" sadfas asddf asdf """
+import sys
+import datetime
+import gzip
+from Bio import SeqIO
+
+
+def usage():
+    """
+    docstring for usage
+    
+    Try me:
+        python test.fast.py /cygene/data/20190712_MG_G22_G23_G27_G28_G29_G30/G30E1L2_BKDL190819904-1a/G30E1L2_BKDL190819904-1a_1.fq.gz
+    """
+    
+def print_current_time():
+    """this is the module docstring """
+    time_stamp = datetime.datetime.now()
+    return time_stamp.strftime('%Y.%m.%d-%H:%M:%S')
+
+
+print '{0} start'.format(print_current_time())
+with gzip.open(sys.argv[1], "r") as inp:
+    NUM_LINES = sum([1 for line in inp])
+print NUM_LINES
+print "%s finished" % print_current_time()
+
+print "%s start" % print_current_time()
+HEADER = gzip.open(sys.argv[1], "r")
+try:
+    NUM_LINES = sum([1 for each in SeqIO.parse(HEADER, "fastq")])
+    print NUM_LINES
+    print "%s start" % print_current_time()
+finally:
+    HEADER.close()
