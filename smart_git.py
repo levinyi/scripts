470fe0b61bba60e81864c389f0a86dfba0bb45df
dushiyi <dushiyi319@163.com>
2019-08-06 15:57:36 +0800

    some changes

--- smart_git.py
+++ smart_git.py
@@ -1,26 +1,21 @@
-"""
-smart git tools 
-"""
-
+"""  smart git tools """
+import sys
+import os
 # Info
 __author__ = 'Shiyi Du'
 
 # Imports
-import sys
-import os 
 
-def git_push(a):
+
+def git_push(ajob):
     """docstring for push_git"""
-    os.system("git add %s"%a)
+    os.system("git add %s" % ajob)
     os.system("git commit -m 'some changes' ")
     os.system("git push origin master")
-    #print "git add ",a
-    #print "git commit -m \"some changes\" "
-    #print "git push origin master"
 
 if __name__ == '__main__':
-    job = sys.argv[1]
-    if not os.path.exists(job):
+    JOB = sys.argv[1]
+    if not os.path.exists(JOB):
         sys.exit("No such file!")
     else:
-        git_push(job)
+        git_push(JOB)
