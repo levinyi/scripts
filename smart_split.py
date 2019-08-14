ba8afde1756bbff37503e09b78ef9600820a6aa3
dushiyi <dushiyi319@163.com>
2019-08-14 08:53:27 +0800

    some changes

--- smart_split.py
+++ smart_split.py
@@ -1,10 +1,18 @@
+""" this module docstring"""
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-a","--total_number",type=int,help="total_number of the file")
-parser.add_argument("-b","--line",type=int,help="how many lines in one.")
-args = parser.parse_args()
 
-for i in range(1,args.total_number):
-    if args.total_number%(args.line*i) == 0:
-        print args.line*i,args.total_number/(args.line*i)
+def main():
+    """docstring for main"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--total_nu", type=int, help="files total numbe")
+    parser.add_argument("-b", "--line", type=int, help="how many lines in one")
+    args = parser.parse_args()
+
+    for i in range(1, args.total_number):
+        if args.total_nu % (args.line*i) == 0:
+            print args.line*i, args.total_nu/(args.line*i)
+
+
+if __name__ == '__main__':
+    main()
