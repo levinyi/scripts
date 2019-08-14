ef0a440609c9bb54b9fdfaa16c3bdca3ac6fec9a
dushiyi <dushiyi319@163.com>
2019-08-14 10:35:30 +0800

    some changes

--- Version.py
+++ Version.py
@@ -2,7 +2,7 @@
 Version and authorship information
 """
 
-__author__    = 'Shiyi Du,'
+__author__    = 'Shiyi Du'
 __copyright__ = 'Copyright 2018 ROOTPATH .cn, Canton. All rights reserved.'
 __license__   = 'Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)'
 __version__   = '0.4.4.999'
