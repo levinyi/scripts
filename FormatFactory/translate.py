636f0d21b1fcec1f81844914f7ca0ae26a5d088f
levin <dushiyi319@163.com>
2020-07-08 08:49:18 +0800

    some changes

---
+++ FormatFactory/translate.py
@@ -0,0 +1,34 @@
+def translate(dna):
+    gencode = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+    }
+
+    amino_acid_sequence = ""
+    for start in range(0,len(dna) - 2, 3):
+        stop = start + 3
+        codon = dna[start:stop]
+        aa = gencode.get(codon.upper(),'X') #当指定键的值不存在时，返回X
+        amino_acid_sequence = amino_acid_sequence + aa
+    return(amino_acid_sequence)
+
+def main():
+    dna = "ATGCGCATCGTACGTCAGTCGAT"
+    print(translate(dna))
+
+if __name__ == '__main__':
+    main()
