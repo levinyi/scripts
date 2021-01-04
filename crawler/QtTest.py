857ace1f48ffdd7fc33caa8442a941d36164d9e6
levin <dushiyi319@163.com>
2021-01-04 16:56:04 +0800

    rm qttest.py

--- crawler/QtTest.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5 import QtWidgets, QtCore
-app = QtWidgets.QApplication(sys.argv)
-widget = QtWidgets.QWidget()
-widget.resize(400, 400)
-widget.setWindowTitle('Hello World')
-widget.show()
-sys.exit(app.exec__())
