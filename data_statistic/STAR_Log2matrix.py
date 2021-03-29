94031969656cd8ba4a5dc186560677bde013339d
levin <dushiyi319@163.com>
2021-03-29 09:14:35 +0800

    some changes

---
+++ data_statistic/STAR_Log2matrix.py
@@ -0,0 +1,21 @@
+import sys
+import re
+
+
+class STARLog:
+    def __init__(self):
+        total_reads = self.total_reads
+        unique_mapped_reads = self.unique_mapped_reads
+        number
+
+
+def deal_with_star_log():
+    alist = []
+    with open() as f:
+        for line in f:
+            line = line.s
+    return alist
+
+for each_file in sys.argv[1:]:
+    name = each_file.split(".")[0]
+    deal_with_star_log()
