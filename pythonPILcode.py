66f5755010953d3b88780ad71a80c220ff7a244b
levin <dushiyi319@163.com>
2022-11-13 16:47:15 +0800

    some changes

---
+++ pythonPILcode.py
@@ -0,0 +1,50 @@
+import random
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+
+def check_code(width=120, height=30, char_length=5, font_file='Monaco.ttf', font_size=28):
+    code = []
+    img = Image.new('RGB', (width, height), (255, 255, 255))
+    draw = ImageDraw.Draw(img, 'RGB')
+
+    def rndChar():
+        return chr(random.randint(65, 90))
+
+    def rndColor():
+        return (random.randint(0, 255), random.randint(0, 255), random.randint(64, 255))
+
+    # 写字符
+    font = ImageFont.truetype(font_file, font_size)
+    for i in range(char_length):
+        char = rndChar()
+        code.append(char)
+        h = random.randint(0, 4)
+        draw.text([i * width/ char_length, h], char, font=font, fill=rndColor())
+
+    # 写干扰点
+    for i in range(40):
+        draw.point((random.randint(0, width), random.randint(0, height)), fill=rndColor())
+
+    # 写干扰圆圈
+    for i in range(40):
+        draw.point((random.randint(0, width), random.randint(0, height)), fill=rndColor())
+        x = random.randint(0, width)
+        y = random.randint(0, height)
+        draw.arc((x, y, x + 4, y + 4), 0, 90, fill=rndColor())
+    
+    # 画干扰线
+    for i in range(5):
+        x1 = random.randint(0, width)
+        y1 = random.randint(0, height)
+        x2 = random.randint(0, width)
+        y2 = random.randint(0, height)
+        draw.line((x1, y1, x2, y2), fill=rndColor())
+
+    img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+    return img, ''.join(code)
+
+
+if __name__ == '__main__':
+    img, code_str = check_code()
+    print(code_str)
+
+    img.save('code.png', 'png')
