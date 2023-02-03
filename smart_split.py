2eccd70f31a0d767c4ccab083dd6c07b8a2b9e92
levin <dushiyi319@163.com>
2023-02-03 10:11:51 +0800

    some changes

--- smart_split.py
+++ smart_split.py
@@ -11,7 +11,7 @@ def main():
 
     for i in range(1, args.total_nu):
         if args.total_nu % (args.line*i) == 0:
-            print args.line*i, args.total_nu/(args.line*i)
+            print(args.line*i, args.total_nu/(args.line*i))
 
 
 if __name__ == '__main__':
